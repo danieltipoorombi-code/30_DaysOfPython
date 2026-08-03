beb28858c245933d001dc818c1166a61672f3338
tipopamungu daniel <tipodan0@gmail.com>
2026-08-03 10:47:49 +0300

    Day_17

---
+++ Day_17.py
@@ -0,0 +1,9 @@
+
+names = ['Finland', 'Sweden', 'Norway','Denmark','Iceland', 'Estonia','Russia']
+#unpack the first 5 countries
+nordic_countries = names[:5]
+es = names[5]
+ru = names[6]
+print('Nordic: ', nordic_countries)
+print('Estonia: ', es)
+print('Russia: ', ru)
